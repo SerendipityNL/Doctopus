6899fe7290239b588bc21bdcb678019db4f84068
Tjerk Dijkstra <tjerk.dijkstra@gmail.com>
2013-05-28 14:48:12 +0200

    functions for edit

--- beta/001/public/js/document_functions.js
+++ beta/001/public/js/document_functions.js
@@ -1,11 +1,11 @@
 
 jQuery(document).ready(function() {
-
-	$('.resize').on('click', function(e) {
-            e.preventDefault();
-            var new_col = $(this).attr('data-setcol');
-            $(this).parent().removeClass();
-            $(this).parent().addClass('col_' + new_col);
-            
-     });
+	$("#workfield-container").on('mouseover', 
+		function () {
+			//$(this).addClass("hover");
+			alert('penis');
+		}).on('mouseout',
+			function () {
+			//$(this).removeClass("hover");
+		});
 });
