9640051f0cf68e0a79e64a67e64bcb44b06db7c4
Douwe de Haan <douwedehaan@live.nl>
2013-05-30 13:18:12 +0200

    [wip] changed some Yellow_Text stuff

--- beta/002/public/js/yellow_text.js
+++ beta/002/public/js/yellow_text.js
@@ -46,7 +46,7 @@
         defaultFontSize    : '1em',
         defaultFontColor   : '#000000',
         defaultActions     : ['bold', 'underline', 'italic', 'strikethrough', 'align-left', 'align-center', 'align-right', 'unordered-list', 'ordered-list', 'link', 'image'],
-        noSubmitButSave	   : false,
+        noSubmitButSave	   : true,
         saveButtonClass    : 'js-editor-save',
         cancelButtonClass  : 'js-editor-cancel',
 
@@ -221,7 +221,7 @@
         
         if( methods.settings.nuSubmitButSave === true ) {
 	        $('<a />').addClass( methods.settings.saveButtonClass ).text( 'Save' ).appendTo( methods.buttons );
-	        $('<a />').addClass( methods.settings.saveButtonClass ).text( 'Save' ).appendTo( methods.buttons );
+	        $('<a />').addClass( methods.settings.cancelButtonClass ).text( 'Cancel' ).appendTo( methods.buttons );
         }
       }
     },
