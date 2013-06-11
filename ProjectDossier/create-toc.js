70bdd859aebac13965e318f915e2426b9924d23c
Vincent Bremer <vbremer89@gmail.com>
2013-06-11 15:14:06 +0200

    TOC is being created, ID's are added

--- ProjectDossier/create-toc.js
+++ ProjectDossier/create-toc.js
@@ -1,17 +1,50 @@
 var list = [];
+var parentCount = 0;
+var childCount = 0;
 
+function mkId(title) {
+	var newTitle = title.toLowerCase().replace(/ /g, '-');
+	return newTitle;
+}
 
-$('h1').each(function() {
-	var title = $(this).text();
-	list.push(title);
+$(':header').each(function() {
+	var tag = $(this).get(0).tagName;
+	if (tag == 'H1' || tag == 'H2') {
+		var title = $(this).text();
+
+		if (tag == 'H1') {
+			childCount = 0;
+			parentCount++;
+			var anchor = parentCount + '-' + mkId(title);
+			$(this).attr('id', anchor);
+
+			list.push({
+				'title': title,
+				'number' : parentCount,
+				'anchor': anchor
+			});
+			
+		}
+		else {
+			childCount++;
+			var anchor = parentCount + '-' + childCount + '-' + mkId(title);
+			$(this).attr('id', anchor);
+
+			list.push({
+				'parent': parentCount,
+				'title': title,
+				'number': parentCount + '.' + childCount,
+				'anchor': anchor
+			});
+		}		
+	}
 });
 
-console.log(list);
+var html = '';
 
-/*
-
-- loop over h1 en h2 heen
-- zet anchor tags in h1/h2
-- text.toLowerCase()
-
-*/
+for (var i = 0; i < list.length; i++) {
+	//console.log(list[i].number + ' - ' + list[i].title);
+	html += '<li><a href="#' + list[i].anchor + '">' + list[i].title + '</a></li>';
+	
+}
+$('.toc').append(html);
