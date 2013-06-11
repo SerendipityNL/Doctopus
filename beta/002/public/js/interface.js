c0cff59e14503b8e6696e2da8126d77250cfdd2c
Douwe de Haan <douwedehaan@live.nl>
2013-06-11 13:33:49 +0200

    [WIP] working on creating documents

--- beta/002/public/js/interface.js
+++ beta/002/public/js/interface.js
@@ -9,4 +9,11 @@ jQuery(document).ready( function() {
 	jQuery('#newDocumentButton').on('click', function() {
 		$('#newDocumentModal').modal('show');
 	});
+	jQuery('#newDocumentModal .modal-footer .btn-success').on('click', function() {
+		var newDocument 		= [];
+		newDocument.title 		= jQuery('#newDocumentModal .modal-body input[name="documentTitle"]').val();
+		newDocument.visibility	= jQuery('#newDocumentModal .modal-body select').val();
+		
+		
+	});
 });
